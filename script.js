b4a095f1014cf30fe7606e68dd45cb1d46f16fe7
Clarence-Coronel <clarence.coronel.r@gmail.com>
2024-01-07 18:15:00 +0800

    Finish website layout

---
+++ script.js
@@ -0,0 +1,9 @@
+function Book(title, author, pageNum, isRead){
+    this.title = title;
+    this.author = author;
+    this.pageNum = pageNum;
+    this.isRead = isRead;
+    this.info = ()=>{
+      return `${this.title} by ${this.author}, ${pageNum} pages, ${isRead ? "has been read" :"not read yet"}.`;
+    }
+  }
